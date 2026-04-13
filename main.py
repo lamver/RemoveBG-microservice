94e12eb184bf6ed38d4e838d74e40bef864ac00a
valeriifrolov@gmail.com <1Q@w3easdzxc>
2026-04-13 14:09:57 +0200

    add use .env

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from fastapi import FastAPI, UploadFile, Response
-from rembg import remove
+from rembg import remove, new_session
 import io
 
 app = FastAPI()
 
+# Попробуем модель 'u2netp' (она более "грубая" и меньше мылит края на графике)
+session = new_session("birefnet-general")
+
 @app.post("/remove-bg")
 async def remove_background(file: UploadFile):
-    # Читаем входящий файл
     input_data = await file.read()
     
-    # Удаляем фон
-    output_data = remove(input_data)
+    # КЛЮЧЕВЫЕ ИЗМЕНЕНИЯ:
+    # 1. Выключаем alpha_matting совсем (он главный виновник мыла на тексте)
+    # 2. Выключаем post_process_mask (он иногда сглаживает углы букв)
+    output_data = remove(
+        input_data,
+        session=session,
+        alpha_matting=False, 
+        post_process_mask=True
+    )
     
-    # Возвращаем результат в формате PNG
     return Response(content=output_data, media_type="image/png")
 
 @app.get("/helth")
